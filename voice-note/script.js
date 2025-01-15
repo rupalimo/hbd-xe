2c1cce82a1599b6a29097a0149eff50baae0cfdc
rupali <36826017+rupalimo@users.noreply.github.com>
2025-01-14 22:06:21 -0500

    udpate styling

--- voice-note/script.js
+++ voice-note/script.js
@@ -1,25 +1,19 @@
 $(document).ready(function () {
 	var playing = false;
 
-	// $('#names-toggle').click(function () {
-	// 	$('p').fadeToggle(250);
-	// });
-
 	$('#play-toggle-1').click(function () {
 		if (playing == false) {
 			document.getElementById('player-1').play();
 			playing = true;
-			$(this).css('background', '#eebbff');
+			$(this).removeClass('btn-default').addClass('btn-playing');
 			$('.name-1').css('display', 'block');
 			$('.hbd-1').css('display', 'none');
 		} else {
 			document.getElementById('player-1').pause();
 			playing = false;
-			$(this).css({
-				'background': '#ebff94',
-				'border-radius': '10rem',
-			});
+			$(this).removeClass('btn-playing').addClass('btn-default');
 			$('.name-1').css('display', 'none');
+			$('.hbd-1').css('display', 'block');
 		}
 	});
 
@@ -27,258 +21,270 @@ $(document).ready(function () {
 		if (playing == false) {
 			document.getElementById('player-2').play();
 			playing = true;
-			$(this).css('background', 'yellow');
+			$(this).removeClass('btn-default').addClass('btn-playing');
 			$('.name-2').css('display', 'block');
 			$('.hbd-2').css('display', 'none');
 		} else {
 			document.getElementById('player-2').pause();
 			playing = false;
-			$(this).css({
-				'background': '#cbddfb',
-				'border-radius': '10rem',
-			});
+			$(this).removeClass('btn-playing').addClass('btn-default');
 			$('.name-2').css('display', 'none');
+			$('.hbd-2').css('display', 'block');
 		}
 	});
 	$('#play-toggle-3').click(function () {
 		if (playing == false) {
 			document.getElementById('player-3').play();
 			playing = true;
-			$(this).css('background', 'yellow');
+			$(this).removeClass('btn-default').addClass('btn-playing');
 			$('.name-3').css('display', 'block');
 			$('.hbd-3').css('display', 'none');
 		} else {
 			document.getElementById('player-3').pause();
 			playing = false;
-			$(this).css({
-				'background': '#b0e0e6',
-				'border-radius': '10rem',
-			});
+			$(this).removeClass('btn-playing').addClass('btn-default');
 			$('.name-3').css('display', 'none');
+			$('.hbd-3').css('display', 'block');
 		}
 	});
 	$('#play-toggle-4').click(function () {
 		if (playing == false) {
 			document.getElementById('player-4').play();
 			playing = true;
-			$(this).css('background', 'yellow');
+			$(this).removeClass('btn-default').addClass('btn-playing');
 			$('.name-4').css('display', 'block');
 			$('.hbd-4').css('display', 'none');
 		} else {
 			document.getElementById('player-4').pause();
 			playing = false;
-			$('.hbd-4').css('display', 'block');
+			$(this).removeClass('btn-playing').addClass('btn-default');
 			$('.name-4').css('display', 'none');
+			$('.hbd-4').css('display', 'block');
 		}
 	});
 	$('#play-toggle-5').click(function () {
 		if (playing == false) {
 			document.getElementById('player-5').play();
 			playing = true;
-			$(this).css('background', 'yellow');
+			$(this).removeClass('btn-default').addClass('btn-playing');
 			$('.name-5').css('display', 'block');
 			$('.hbd-5').css('display', 'none');
 		} else {
 			document.getElementById('player-5').pause();
 			playing = false;
-			$('.hbd-5').css('display', 'block');
+			$(this).removeClass('btn-playing').addClass('btn-default');
 			$('.name-5').css('display', 'none');
+			$('.hbd-5').css('display', 'block');
 		}
 	});
 	$('#play-toggle-6').click(function () {
 		if (playing == false) {
 			document.getElementById('player-6').play();
 			playing = true;
-			$(this).css('background', 'yellow');
+			$(this).removeClass('btn-default').addClass('btn-playing');
 			$('.name-6').css('display', 'block');
 			$('.hbd-6').css('display', 'none');
 		} else {
 			document.getElementById('player-6').pause();
 			playing = false;
-			$('.hbd-6').css('display', 'block');
+			$(this).removeClass('btn-playing').addClass('btn-default');
 			$('.name-6').css('display', 'none');
+			$('.hbd-6').css('display', 'block');
 		}
 	});
 	$('#play-toggle-7').click(function () {
 		if (playing == false) {
 			document.getElementById('player-7').play();
 			playing = true;
-			$(this).css('background', 'yellow');
+			$(this).removeClass('btn-default').addClass('btn-playing');
 			$('.name-7').css('display', 'block');
 			$('.hbd-7').css('display', 'none');
 		} else {
 			document.getElementById('player-7').pause();
 			playing = false;
-			$('.hbd-7').css('display', 'block');
+			$(this).removeClass('btn-playing').addClass('btn-default');
 			$('.name-7').css('display', 'none');
+			$('.hbd-7').css('display', 'block');
 		}
 	});
 	$('#play-toggle-8').click(function () {
 		if (playing == false) {
 			document.getElementById('player-8').play();
 			playing = true;
-			$(this).css('background', 'yellow');
+			$(this).removeClass('btn-default').addClass('btn-playing');
 			$('.name-8').css('display', 'block');
 			$('.hbd-8').css('display', 'none');
 		} else {
 			document.getElementById('player-8').pause();
 			playing = false;
-			$('.hbd-8').css('display', 'block');
+			$(this).removeClass('btn-playing').addClass('btn-default');
 			$('.name-8').css('display', 'none');
+			$('.hbd-8').css('display', 'block');
 		}
 	});
 	$('#play-toggle-9').click(function () {
 		if (playing == false) {
 			document.getElementById('player-9').play();
 			playing = true;
-			$(this).css('background', 'yellow');
+			$(this).removeClass('btn-default').addClass('btn-playing');
 			$('.name-9').css('display', 'block');
 			$('.hbd-9').css('display', 'none');
 		} else {
 			document.getElementById('player-9').pause();
 			playing = false;
-			$('.hbd-9').css('display', 'block');
+			$(this).removeClass('btn-playing').addClass('btn-default');
 			$('.name-9').css('display', 'none');
+			$('.hbd-9').css('display', 'block');
 		}
 	});
 	$('#play-toggle-10').click(function () {
 		if (playing == false) {
 			document.getElementById('player-10').play();
 			playing = true;
-			$(this).css('background', 'yellow');
+			$(this).removeClass('btn-default').addClass('btn-playing');
 			$('.name-10').css('display', 'block');
 			$('.hbd-10').css('display', 'none');
 		} else {
 			document.getElementById('player-10').pause();
 			playing = false;
-			$('.hbd-10').css('display', 'block');
+			$(this).removeClass('btn-playing').addClass('btn-default');
 			$('.name-10').css('display', 'none');
+			$('.hbd-10').css('display', 'block');
 		}
 	});
 	$('#play-toggle-11').click(function () {
 		if (playing == false) {
 			document.getElementById('player-11').play();
 			playing = true;
-			$(this).css('background', 'yellow');
+			$(this).removeClass('btn-default').addClass('btn-playing');
 			$('.name-11').css('display', 'block');
 			$('.hbd-11').css('display', 'none');
 		} else {
 			document.getElementById('player-11').pause();
 			playing = false;
-			$('.hbd-11').css('display', 'block');
+			$(this).removeClass('btn-playing').addClass('btn-default');
 			$('.name-11').css('display', 'none');
+			$('.hbd-11').css('display', 'block');
 		}
 	});
 	$('#play-toggle-12').click(function () {
 		if (playing == false) {
 			document.getElementById('player-12').play();
 			playing = true;
-			$(this).css('background', 'yellow');
+			$(this).removeClass('btn-default').addClass('btn-playing');
 			$('.name-12').css('display', 'block');
 			$('.hbd-12').css('display', 'none');
 		} else {
 			document.getElementById('player-12').pause();
 			playing = false;
-			$('.hbd-12').css('display', 'block');
+			$(this).removeClass('btn-playing').addClass('btn-default');
 			$('.name-12').css('display', 'none');
+			$('.hbd-12').css('display', 'block');
 		}
 	});
 	$('#play-toggle-13').click(function () {
 		if (playing == false) {
 			document.getElementById('player-13').play();
 			playing = true;
-			$(this).css('background', 'yellow');
+			$(this).removeClass('btn-default').addClass('btn-playing');
 			$('.name-13').css('display', 'block');
 			$('.hbd-13').css('display', 'none');
 		} else {
 			document.getElementById('player-13').pause();
 			playing = false;
-			$('.hbd-13').css('display', 'block');
+			$(this).removeClass('btn-playing').addClass('btn-default');
 			$('.name-13').css('display', 'none');
+			$('.hbd-13').css('display', 'block');
 		}
 	});
 	$('#play-toggle-14').click(function () {
 		if (playing == false) {
 			document.getElementById('player-14').play();
 			playing = true;
-			$(this).css('background', 'yellow');
+			$(this).removeClass('btn-default').addClass('btn-playing');
 			$('.name-14').css('display', 'block');
 			$('.hbd-14').css('display', 'none');
 		} else {
 			document.getElementById('player-14').pause();
 			playing = false;
-			$('.hbd-14').css('display', 'block');
+			$(this).removeClass('btn-playing').addClass('btn-default');
 			$('.name-14').css('display', 'none');
+			$('.hbd-14').css('display', 'block');
 		}
 	});
 	$('#play-toggle-15').click(function () {
 		if (playing == false) {
 			document.getElementById('player-15').play();
 			playing = true;
-			$(this).css('background', 'yellow');
+			$(this).removeClass('btn-default').addClass('btn-playing');
 			$('.name-15').css('display', 'block');
 			$('.hbd-15').css('display', 'none');
 		} else {
 			document.getElementById('player-15').pause();
 			playing = false;
-			$('.hbd-15').css('display', 'block');
+			$(this).removeClass('btn-playing').addClass('btn-default');
 			$('.name-15').css('display', 'none');
+			$('.hbd-15').css('display', 'block');
 		}
 	});
 	$('#play-toggle-16').click(function () {
 		if (playing == false) {
 			document.getElementById('player-16').play();
 			playing = true;
-			$(this).css('background', 'yellow');
+			$(this).removeClass('btn-default').addClass('btn-playing');
 			$('.name-16').css('display', 'block');
 			$('.hbd-16').css('display', 'none');
 		} else {
 			document.getElementById('player-16').pause();
 			playing = false;
-			$('.hbd-16').css('display', 'block');
+			$(this).removeClass('btn-playing').addClass('btn-default');
 			$('.name-16').css('display', 'none');
+			$('.hbd-16').css('display', 'block');
 		}
 	});
 	$('#play-toggle-17').click(function () {
 		if (playing == false) {
 			document.getElementById('player-17').play();
 			playing = true;
-			$(this).css('background', 'yellow');
+			$(this).removeClass('btn-default').addClass('btn-playing');
 			$('.name-17').css('display', 'block');
 			$('.hbd-17').css('display', 'none');
 		} else {
 			document.getElementById('player-17').pause();
 			playing = false;
-			$('.hbd-17').css('display', 'block');
+			$(this).removeClass('btn-playing').addClass('btn-default');
 			$('.name-17').css('display', 'none');
+			$('.hbd-17').css('display', 'block');
 		}
 	});
 	$('#play-toggle-18').click(function () {
 		if (playing == false) {
 			document.getElementById('player-18').play();
 			playing = true;
-			$(this).css('background', 'yellow');
+			$(this).removeClass('btn-default').addClass('btn-playing');
 			$('.name-18').css('display', 'block');
 			$('.hbd-18').css('display', 'none');
 		} else {
 			document.getElementById('player-18').pause();
 			playing = false;
-			$('.hbd-18').css('display', 'block');
+			$(this).removeClass('btn-playing').addClass('btn-default');
 			$('.name-18').css('display', 'none');
+			$('.hbd-18').css('display', 'block');
 		}
 	});
 	$('#play-toggle-19').click(function () {
 		if (playing == false) {
 			document.getElementById('player-19').play();
 			playing = true;
-			$(this).css('background', 'yellow');
+			$(this).removeClass('btn-default').addClass('btn-playing');
 			$('.name-19').css('display', 'block');
 			$('.hbd-19').css('display', 'none');
 		} else {
 			document.getElementById('player-19').pause();
 			playing = false;
-			$('.hbd-19').css('display', 'block');
+			$(this).removeClass('btn-playing').addClass('btn-default');
 			$('.name-19').css('display', 'none');
+			$('.hbd-19').css('display', 'block');
 		}
 	});
 });
